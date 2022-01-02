b5ff236247d3dd5abd56a1bd70de2b3f9c422939
Fisstouk <90407928+Fisstouk@users.noreply.github.com>
2022-01-02 16:18:17 +0100

    V1 avec argument

---
+++ automatic_ssh_arg.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#prenom:lyronn
+#
+#version:1.0
+
+if [ "$#" -ne 1 ]; then
+	echo 'Utilisation: '$0' [chemin_cle_publique.pub]'
+	exit
+fi
+
+#echo "Installation de l'outil sshpass"
+sudo apt install sshpass
+
+#utiliser la cle publique comme argument 1
+file="$1"
+
+echo "Entrez le nom de l'utilisateur de la machine distante"
+read usr
+
+echo "Entrez l'adresse IP de la machine distante"
+read host
+
+#argument -s empeche l'affichage du mot de passe entré
+echo "Entrez le mot de passe de la machine distante"
+read -s password
+
+echo "Transfert de la clé publique au serveur"
+sshpass -p "$password" ssh-copy-id -i "$file" "$usr"@"$host" -p 22
+
+#suppression du contenu de la variable $password
+unset password
+
+echo "Connexion à la machine $usr@$host"
+ssh $usr@$host
+
